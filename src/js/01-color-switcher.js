2367cdff381e83698b67032bb8b0b64b0f7c9847
Tin Pasco <tinontengco@gmail.com>
2024-02-29 21:18:59 +0800

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 167777215.toString(16))}`;
+    //#123456
+}
 
+const body = document.querySelector('body')
+const startButn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+let colorChangeInterval = null;
+stopBtn.disabled = true;
+
+function startColorChange() {
+    colorChangeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+    }, 1000);
+}
+
+function stopColorChange() {
+    
+}
